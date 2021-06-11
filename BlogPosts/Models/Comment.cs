f327cc6fa339e5a72fb5a18aa3affdc628a0976b
mayad-mohamed <mmyada7@gmail.com>
2021-06-11 17:31:49 +0200

    Add project files.

---
+++ BlogPosts/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BlogPosts.Models
+{
+    public partial class Comment
+    {
+        public int CommentId { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime? CommentDate { get; set; }
+        public int? PostId { get; set; }
+
+        public virtual Post Post { get; set; }
+    }
+}
---
+++ BlogPosts/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BlogPosts.Models
+{
+    public partial class Post
+    {
+        public Post()
+        {
+            Comments = new HashSet<Comment>();
+        }
+
+        public int PostId { get; set; }
+        public string PostContent { get; set; }
+        public DateTime? PostDate { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
